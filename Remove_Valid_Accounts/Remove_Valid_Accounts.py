af1e73a3838e710e0f92b0c15c8414c9f03c55c3
Spydr <101361802+spydrful@users.noreply.github.com>
2023-11-13 19:04:15 -0800

    Update Remove_Valid_Accounts.py
    Refactor of code. Added Spur IP scanning to detect if an IP is bad.

--- Remove_Valid_Accounts/Remove_Valid_Accounts.py
+++ Remove_Valid_Accounts/Remove_Valid_Accounts.py
@@ -1,51 +1,69 @@
 import csv
-from collections import defaultdict as dd
+import requests
+from collections import defaultdict
 import ipaddress
 
-
-# Function to check if an IP address is private as for some reason Datadog will report that
 def is_private_ip(ip):
     try:
         ip_obj = ipaddress.ip_address(ip)
         return ip_obj.is_private
     except ValueError:
-        # If the IP address is invalid, treat it as non-private
         return False
 
+def get_risk_for_ip(ip, token):
+    api_url = f"https://api.spur.us/v2/context/{ip}"
+    headers = {"Token": token}
 
-# Input and output file paths
-input_file_path = 'SecondAttack.csv'
-output_file_path = 'output_file.csv'
+    try:
+        response = requests.get(api_url, headers=headers)
+        response.raise_for_status()
+        json_response = response.json()
+        risk_field = json_response.get("risks", [])
+        print(risk_field)
+        return risk_field
+    except Exception as e:
+        print(f"Error making API call for IP {ip}: {e}")
+        return []
 
-# Dictionary to store counts of each non-private IP address
-ip_count = dd(int)
+input_file_path = "SecondAttack.csv"
+output_file_path = "output_file_with_risk.csv"
+api_token = "These are not the tokens you are looking for"
 
-# Read the CSV file and update non-private IP counts
-with open(input_file_path, 'r', newline='') as csvfile:
+ip_count = defaultdict(int)
+
+with open(input_file_path, "r", newline="") as csvfile:
     reader = csv.DictReader(csvfile)
+    rows = list(reader)  # Store rows in a list for further iteration
 
-    for row in reader:
-        ip_list = row['context.ip'].split(',')
+    for row in rows:
+        ip_list = row["context.ip"].split(",")
+        risk_fields = []
 
         for ip in ip_list:
             ip = ip.strip()
             if not is_private_ip(ip):
                 ip_count[ip] += 1
 
-# Filter rows based on non-private IP count and write to a new CSV file
-with open(input_file_path, 'r', newline='') as csvfile, open(output_file_path, 'w', newline='') as outputfile:
-    reader = csv.DictReader(csvfile)
-    fieldnames = reader.fieldnames
+                try:
+                    risk_field = get_risk_for_ip(ip, api_token)
+                    risk_fields.extend(risk_field)
+                except Exception as e:
+                    print(f"Error making API call for IP {ip}: {e}")
+
+        row["Risk"] = ", ".join(risk_fields)
+
+# Reopen the input file and reinitialize the reader object
+csvfile = open(input_file_path, "r", newline="")
+reader = csv.DictReader(csvfile)
+
+with open(output_file_path, "w", newline="") as outputfile:
+    fieldnames = reader.fieldnames + ["Risk"]
     writer = csv.DictWriter(outputfile, fieldnames=fieldnames)
 
-    # Write headers to the output file
     writer.writeheader()
 
-    for row in reader:
-        ip_list = row['context.ip'].split(',')
-
-        # Check if any non-private IP in the row has count greater than 3
-        if any(ip_count[ip.strip()] > 3 for ip in ip_list):
-            writer.writerow(row)
+    # Write rows from the stored 'rows' list
+    for row in rows:
+        writer.writerow(row)
 
 print("Processing complete. Output written to", output_file_path)
